9c030fb52db7410a2d261b57cc2f192ea49b358a
lebenoa <dubi.db28@gmail.com>
2025-09-12 19:29:25 +0700

    Feat: Probably faster one shot (less allocation)

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod error;
 mod response;
 
-use reqwest::{Client, ClientBuilder, StatusCode};
+use std::collections::HashMap;
+
+use reqwest::{Client, ClientBuilder, StatusCode, Url};
 
 use crate::{error::Error, response::APIResponse};
 
-const DEFAULT_USER_AGENT: &str = "Bun/1.2.21";
+pub const DEFAULT_USER_AGENT: &str = "Bun/1.2.21";
 
 pub struct VoucherClient {
     http_client: Client,
@@ -13,36 +15,35 @@ pub struct VoucherClient {
 }
 
 impl VoucherClient {
-    /// Create new Voucher client
+    /// Construct usable, default `reqwest::ClientBuilder`. Perfect for sharing across all `VoucherClient`
+    pub fn new_http_client_builder(user_agent: &str) -> ClientBuilder {
+        ClientBuilder::new().user_agent(user_agent).use_rustls_tls()
+    }
+
+    /// Create new voucher client with new `reqwest::Client`  
+    /// For multiple voucher client, consider using `VoucherClient::new_http_client_builder` and calling `VoucherClient::new_with_client` instead
     ///
     /// # Example
     ///
     /// ```rs
     /// VoucherClient::new("0669991111", Some("custom user agent"));
     /// ```
-    pub fn new<S: Into<String>>(mobile: S, user_agent: Option<S>) -> Result<Self, Error> {
-        let ua = if let Some(u) = user_agent {
-            u.into()
+    pub fn new<S: Into<String>>(mobile: S, user_agent: Option<&str>) -> Result<Self, Error> {
+        let mut http_client_builder = if let Some(u) = user_agent {
+            Self::new_http_client_builder(u)
         } else {
-            DEFAULT_USER_AGENT.to_string()
+            Self::new_http_client_builder(DEFAULT_USER_AGENT)
         };
 
         #[cfg(debug_assertions)]
-        let http_client = ClientBuilder::new()
-            .danger_accept_invalid_certs(true)
-            .danger_accept_invalid_hostnames(true)
-            .user_agent(ua)
-            .use_rustls_tls()
-            .build()?;
-
-        #[cfg(not(debug_assertions))]
-        let http_client = ClientBuilder::new()
-            .user_agent(ua)
-            .use_rustls_tls()
-            .build()?;
+        {
+            http_client_builder = http_client_builder
+                .danger_accept_invalid_certs(true)
+                .danger_accept_invalid_hostnames(true)
+        }
 
         Ok(VoucherClient {
-            http_client,
+            http_client: http_client_builder.build()?,
             mobile: mobile.into(),
         })
     }
@@ -62,7 +63,11 @@ impl VoucherClient {
     ///    .build()
     ///    .unwrap();
     ///
-    /// VoucherClient::new(http_client.clone(), "0669991111")
+    /// // or
+    ///
+    /// let http_client = VoucherClient::new_http_client_builder().build().unwrap();
+    ///
+    /// VoucherClient::new_with_client(http_client.clone(), "0669991111")
     /// ```
     pub fn new_with_client<S: Into<String>>(client: Client, mobile: S) -> Self {
         VoucherClient {
@@ -74,7 +79,7 @@ impl VoucherClient {
     pub async fn redeem(&self, voucher: &str) -> Result<APIResponse, Error> {
         let extracted_id = Self::extract_id(voucher);
         let url = format!("https://gift.truemoney.com/campaign/vouchers/{extracted_id}/redeem");
-        let body = self.create_redeem_request_body(extracted_id);
+        let body = self.create_redeem_request_body(&extracted_id);
         let response = self
             .http_client
             .post(&url)
@@ -83,31 +88,7 @@ impl VoucherClient {
             .send()
             .await?;
 
-        let status = response.status();
-        if status == StatusCode::FORBIDDEN {
-            return Err(Error::Forbidden);
-        }
-
-        let body = response.text().await?;
-        let api_response: APIResponse = match serde_json::from_str(&body) {
-            Ok(a) => a,
-            Err(e) => {
-                return Err(Error::Deserialize(e, body));
-            }
-        };
-
-        if status != StatusCode::OK {
-            match status {
-                _ if !api_response.status.code().is_success() => {
-                    return Err(Error::Voucher(Box::new(api_response)));
-                }
-                _ => {
-                    return Err(Error::StatusCode(status.as_u16(), body));
-                }
-            }
-        }
-
-        Ok(api_response)
+        Self::handle_response(response).await
     }
 
     pub async fn verify(&self, voucher: &str) -> Result<APIResponse, Error> {
@@ -116,6 +97,7 @@ impl VoucherClient {
             "https://gift.truemoney.com/campaign/vouchers/{extracted_id}/verify?mobile={}",
             self.mobile
         );
+
         let response = self
             .http_client
             .get(&url)
@@ -123,6 +105,81 @@ impl VoucherClient {
             .send()
             .await?;
 
+        Self::handle_response(response).await
+    }
+
+    pub async fn one_shot_redeem(mobile: &str, voucher: &str) -> Result<APIResponse, Error> {
+        let extracted_id = Self::extract_id(voucher);
+        let url = format!("https://gift.truemoney.com/campaign/vouchers/{extracted_id}/redeem");
+        let body = format!(r#"{{"mobile":"{mobile}","voucher_hash":"{extracted_id}"}}"#,);
+        let client = Self::new_http_client_builder(DEFAULT_USER_AGENT).build()?;
+
+        let response = client
+            .post(&url)
+            .header("Content-Type", "application/json")
+            .body(body)
+            .send()
+            .await?;
+
+        Self::handle_response(response).await
+    }
+
+    pub async fn one_shot_redeem_with_client(
+        client: Client,
+        mobile: &str,
+        voucher: &str,
+    ) -> Result<APIResponse, Error> {
+        let extracted_id = Self::extract_id(voucher);
+        let url = format!("https://gift.truemoney.com/campaign/vouchers/{extracted_id}/redeem");
+        let body = format!(r#"{{"mobile":"{mobile}","voucher_hash":"{extracted_id}"}}"#,);
+
+        let response = client
+            .post(&url)
+            .header("Content-Type", "application/json")
+            .body(body)
+            .send()
+            .await?;
+
+        Self::handle_response(response).await
+    }
+
+    pub async fn one_shot_verify(mobile: &str, voucher: &str) -> Result<APIResponse, Error> {
+        let extracted_id = Self::extract_id(voucher);
+        let url = format!(
+            "https://gift.truemoney.com/campaign/vouchers/{extracted_id}/verify?mobile={mobile}",
+        );
+        let client = Self::new_http_client_builder(DEFAULT_USER_AGENT).build()?;
+
+        let response = client
+            .get(&url)
+            .header("Content-Type", "application/json")
+            .send()
+            .await?;
+
+        Self::handle_response(response).await
+    }
+
+    pub async fn one_shot_verify_with_client(
+        client: Client,
+        mobile: &str,
+        voucher: &str,
+    ) -> Result<APIResponse, Error> {
+        let extracted_id = Self::extract_id(voucher);
+        let url = format!(
+            "https://gift.truemoney.com/campaign/vouchers/{extracted_id}/verify?mobile={mobile}",
+        );
+
+        let response = client
+            .get(&url)
+            .header("Content-Type", "application/json")
+            .send()
+            .await?;
+
+        Self::handle_response(response).await
+    }
+
+    #[inline]
+    async fn handle_response(response: reqwest::Response) -> Result<APIResponse, Error> {
         let status = response.status();
         if status == StatusCode::FORBIDDEN {
             return Err(Error::Forbidden);
@@ -150,41 +207,18 @@ impl VoucherClient {
         Ok(api_response)
     }
 
-    pub async fn one_shot_redeem(mobile: &str, voucher: &str) -> Result<APIResponse, Error> {
-        let vc = VoucherClient::new(mobile, None)?;
-        vc.redeem(voucher).await
-    }
-
-    pub async fn one_shot_redeem_with_client(
-        client: Client,
-        mobile: &str,
-        voucher: &str,
-    ) -> Result<APIResponse, Error> {
-        let vc = VoucherClient::new_with_client(client, mobile);
-        vc.redeem(voucher).await
-    }
-
-    pub async fn one_shot_verify(mobile: &str, voucher: &str) -> Result<APIResponse, Error> {
-        let vc = VoucherClient::new(mobile, None)?;
-        vc.verify(voucher).await
-    }
-
-    pub async fn one_shot_verify_with_client(
-        client: Client,
-        mobile: &str,
-        voucher: &str,
-    ) -> Result<APIResponse, Error> {
-        let vc = VoucherClient::new_with_client(client, mobile);
-        vc.verify(voucher).await
-    }
-
     #[inline]
-    fn extract_id(link: &str) -> &str {
-        match link.split_once("?v=") {
-            Some((_prefix, id)) => id,
-            None => link,
+    fn extract_id(link: &str) -> String {
+        if let Ok(url) = Url::parse(link) {
+            let mut queries: HashMap<_, _> = url.query_pairs().into_owned().collect();
+            if let Some(v) = queries.remove("v") {
+                return v;
+            }
         }
+
+        link.to_string()
     }
+
     #[inline]
     fn create_redeem_request_body(&self, voucher_id: &str) -> String {
         format!(
